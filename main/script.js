ffe8a96f8fe403aa98822cc7b4ceab59f96ee5e0
Omar <152491762+Omar-i9@users.noreply.github.com>
2026-04-14 19:26:54 +0300

    Update script.js

--- main/script.js
+++ main/script.js
@@ -1282,7 +1282,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function toggleAyahExpanded(nextState) {
     ayahExpanded = nextState;
     if (ayahFloat) ayahFloat.classList.toggle("expanded", ayahExpanded);
-    ayahDelay = ayahExpanded ? 10000 : 5000;
+    ayahDelay = ayahExpanded ? 13000 : 6000;
     restartAyahRotation();
   }
 
